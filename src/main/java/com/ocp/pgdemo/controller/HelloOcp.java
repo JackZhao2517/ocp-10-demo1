dbd1a778b0e254040dac15d3f35e9b5b1b3d3b5f
JackZhao2517 <2517dream@163.com>
2020-03-31 21:19:27 +0800

    first commit

---
+++ src/main/java/com/ocp/pgdemo/controller/HelloOcp.java
@@ -0,0 +1,27 @@
+package com.ocp.pgdemo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ocp.pgdemo.model.RspModel;
+
+@RestController
+public class HelloOcp {
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello() for oc exercise 2!";
+	}
+
+	@GetMapping("/api/v1/hello")
+	public ResponseEntity<RspModel> helloRes(){
+		RspModel rsp = new RspModel();
+		rsp.setCode(200);
+		rsp.setData("HelloRes() for oc exercise 2!");
+		
+		return new ResponseEntity<RspModel>(rsp, HttpStatus.OK);
+		
+	}
+
+}
---
+++ src/main/java/com/ocp/pgdemo/model/RspModel.java
@@ -0,0 +1,45 @@
+package com.ocp.pgdemo.model;
+
+public class RspModel {
+	private Integer code;
+	private String msg;
+	private Object data;
+	/**
+	 * @return the code
+	 */
+	public Integer getCode() {
+		return code;
+	}
+	/**
+	 * @param code the code to set
+	 */
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+	/**
+	 * @return the msg
+	 */
+	public String getMsg() {
+		return msg;
+	}
+	/**
+	 * @param msg the msg to set
+	 */
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	/**
+	 * @return the data
+	 */
+	public Object getData() {
+		return data;
+	}
+	/**
+	 * @param data the data to set
+	 */
+	public void setData(Object data) {
+		this.data = data;
+	}
+	
+
+}
