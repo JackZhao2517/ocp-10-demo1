046bcd798c79029bf06fa22c172d4e3f444a1508
JackZhao2517 <2517dream@163.com>
2020-04-15 19:57:54 +0800

    profile change test

--- src/main/java/com/ocp/pgdemo/controller/HelloOcp.java
+++ src/main/java/com/ocp/pgdemo/controller/HelloOcp.java
@@ -1,11 +1,13 @@
 package com.ocp.pgdemo.controller;
 
 import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ocp.pgdemo.model.RspModel;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class HelloOcp {
@@ -13,12 +15,15 @@ public class HelloOcp {
 	public String hello() {
 		return "Hello() for oc exercise 2!_update";
 	}
+	@Value("${hello}")
+	private String hello;
 
 	@GetMapping("/api/v1/hello")
 	public ResponseEntity<RspModel> helloRes(){
 		RspModel rsp = new RspModel();
 		rsp.setCode(200);
-		rsp.setData("HelloRes() for oc exercise 2_update!");
+		//rsp.setData("HelloRes() for oc exercise 2_update!");
+		rsp.setData(hello);
 		
 		return new ResponseEntity<RspModel>(rsp, HttpStatus.OK);
 		
